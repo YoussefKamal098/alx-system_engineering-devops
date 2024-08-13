f0862e0aa1099026b73e09702f77013a90905f9e
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-13 22:13:46 +0300

    100-count.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This Module contains a recursive function that queries the
+Reddit API and returns a list containing the titles of all
+hot articles for a given subreddit. If no results are found for
+the given subreddit, the function should return None.
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Recursive function that queries the Reddit API and returns a
+    list containing the titles of all hot articles for a given subreddit.
+    If no results are found for the given subreddit,
+    the function should return None.
+    """
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Linux"},
+        params={"after": after},
+    )
+
+    if response.status_code != 200:
+        return None
+
+    for children in response.json().get("data").get("children"):
+        hot_list.append(children.get("data").get("title"))
+
+    print(after)
+
+    after = response.json().get("data").get("after")
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, after)
