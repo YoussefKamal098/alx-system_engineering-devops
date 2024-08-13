b6e748188b588a6fdef3453a821ac828fb98c2db
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-13 22:14:19 +0300

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+this module contains a function that queries the
+Reddit API and prints the titles of the first 10 hot
+posts listed for a given subreddit or None if invalid subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit or
+    None if invalid subreddit.
+    """
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Linux"},
+        params={"limit": 10},
+    )
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    for children in response.json().get("data").get("children"):
+        print(children.get("data").get("title"))
+
+
+top_ten('programming')
